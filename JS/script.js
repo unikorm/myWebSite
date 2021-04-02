6aed34d4d491b5836cac30f0770dc78a6577ace4
adamko <adaled00@gmail.com>
2021-04-02 22:15:36 +0200

    uprava suborov, pokracovanie ucenia JS

--- JS/script.js
+++ JS/script.js
@@ -17,7 +17,7 @@
 // ked chiti otvarim usta ( nejaka animacia mojej hlavy ) ... asi to pojde do nekonecna, hore bude nejaky odpocet
 // niekde na stranku napisem ze stranka obsahuje easter egg a user ho bude musiet najst
 
-document.body.addEventListener("keypres", function keyboardedIt(event){
+/* document.body.addEventListener("keypres", function keyboardedIt(event){
     var oldLeft = getComputedStyle(document.body ).left,
         newLeft;
         oldLeft = parseInt(oldLeft, 10); //50px -> 50, ta 10 je ze v ktorej sustave robime ( tu je to teda desiatkova )
@@ -28,4 +28,65 @@ document.body.addEventListener("keypres", function keyboardedIt(event){
             newLeft = oldLeft + 10; // 60
         } 
         document.body.style.left = newLeft + "px"
-    })
+    }) */
+
+
+    //uloha number 9
+   
+$(function(){
+    var image = $("img"); //toto najde img element
+
+    //na to, aby na elementy fungovali hodnoty "left", position musi byt iny ako static
+    image.css({ position: "relative"});
+
+    //jednoducha verzia, kde img iba odleti 
+    //image.animate({ left: 500}, 2000 );
+
+    //toto je komplikovanejsia verzia, musime nan kliknut a az potom poleti
+    image.on("click", function(){
+        image.animate({ left: $("html").width()}, 2000); 
+    });
+    var siteHeight = $("body").height();
+    console.log(siteHeight);
+}); 
+
+
+    //uloha number 10
+
+
+let = Number(window.prompt("napis sem cislo", ""));
+var numberOne = Number(prompt("enter any number", ""));
+var numberTwo = Number(prompt("enter another number", ""));
+console.log(numberOne);
+console.log(numberTwo);
+
+$(function(){
+    var resulto = numberOne + numberTwo;
+    //console.log(resulto);
+    alert("your number is " + resulto);
+}) 
+
+
+
+//uloha number 11
+
+
+var uno = Number(prompt("enter number", ""));
+
+$(function() {
+    var duo = uno * 2;
+    console.log(duo);
+}) 
+
+function vratCislo(uno){
+    return uno * 3;
+}
+
+var vrateneCislo = vratCislo(uno);
+
+console.log(vrateneCislo); 
+
+
+
+
+
