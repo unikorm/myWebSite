a2ab423ab8514d4f80805db69babbeea846a10c8
adamko <adaled00@gmail.com>
2021-07-07 21:58:38 +0200

    zacal JS prechodom na zaciatok plochy pre hru a ukoncenim prechodu....

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,25 @@
 }); */
 
 $(function() {
-        
+        var input = $("#clickToGame");
+        var screen = $("body");
+        var gameCentre = $("<div/>", { id: "gameCentre"});
+                gameCentre.appendTo("body").hide();
+        input.on("click", function() {
+                screen.animate({
+                        opacity: ".5",
+                        width: "99%",
+                        height: "95%"
+                }, 500 );
+                gameCentre.show(600);
+                event.preventDefault();
+        });
+
+        $(document).on("keyup", function(event) {
+                if (event.which === 27) gameCentre.hide();
+                                        screen.animate({ opacity: 1, width: "98%", height: "96%" }, 100);
+        })
+
 });
 
 
