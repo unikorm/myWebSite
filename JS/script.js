8f57a12de1e090e2348cba849522c94891e2a493
adamko <adaled00@gmail.com>
2021-07-20 22:00:16 +0200

    ending reconecting game and web for now, until finish game

--- JS/script.js
+++ JS/script.js
@@ -6,33 +6,48 @@
 }); */
 
 $(function() {
+        //main variables
         var input = $("#clickToGame");
         var gameCentre = $("#gameCentre");
                          gameCentre.hide();
         var gameIntro = $("#gameIntro");
                          gameIntro.hide();
+        var playButton = $("#playButton");
 
+        // on click on my logo open the main menu of game
         input.on("click", function(event) {
                 openGame();
                 openGameMenu();
                 event.preventDefault();
         });
 
-        function openGame(){
-                gameCentre.show(700);
-        };
+        // on click on play button open playground
+        playButton.on("click", function() {
+                openPlayGround();
+        });
 
+        // open background of game
+        function openGame(){
+                gameCentre.show(600);
+        };
+        // open menu of game
         function openGameMenu(){
                 setTimeout( function() {
-                        gameIntro.show(400);
+                        gameIntro.show(10);
                 }, 700);
-        }
+        };
+
+        // open playground on click "play"
+        function openPlayGround(){
+                // tu bude kod na otvorenie playground ked to spojim s myGame vetvou... tu mam zatial rozrobenu 
+        };
 
 
+        // on "esc" click close the game
         $(document).on("keyup", function(event) {
                 if (event.which === 27) gameCentre.hide(700);
-                                        gameIntro.hide(100);
-        })
+                if (event.which === 27) gameIntro.hide(100);
+        });
 
 });
 
