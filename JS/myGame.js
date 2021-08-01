1b1b095b2d2be8ff5e396366f1b22506698b95a3
adamko <adaled00@gmail.com>
2021-08-01 20:57:24 +0200

    all in one...

---
+++ JS/myGame.js
@@ -0,0 +1,30 @@
+
+
+// canvas
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+// game
+const titleSize = 50;
+const tileCountX = canvas.width / titleSize;
+const tileCountY = canvas.height / titleSize;
+
+
+
+
+drawStuff();
+
+// draw stuff
+function drawStuff() {
+    // background
+    rectangle("#edf5e1", 0, 0, canvas.width, canvas.height);
+
+
+    // draw rectangle
+    function rectangle(color, x, y, width, height) {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, width, height);
+    }
+
+}
+
